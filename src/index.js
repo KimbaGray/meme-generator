83e4f2d922c02d2ef0ac334716dc51f1f27deab3
Kim Gray <kim.lc.gray@gmail.com>
2020-04-04 22:06:30 +0100

    added favicon

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Favicon from "react-favicon";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Favicon url="https://previews.dropbox.com/p/thumb/AAwGtTCxcWrsRLFi-VzU-GT4k-9W1kPoK26h_4mE9zD4Sc2RLu8i_RxrMx2Wm_2zzE3TTAsLFWOEDRj8KHoSvGW56a0jpjhTEdroXxTzW7MC_axDI4qc35fbkuD4-LcVsbmaQhHWzQYJw99icNLiYtOsFXrlcvHZTsN5gGxyQeFBnud7YJ-TC2mcfGJBEymrKzSuFdGwjwqX493XDLLXvtpKCSMyomyWYTM2fi_-5T8UIiXleF4PmZCrPJBi9HOnnWrrftnZz-Mgt9PIuU7JQUygMQrsrEqXwUVmrUXFWKxi4lRKtVWTzkB6ONe2cHlscD488blYkHitXwxwnHoTSsSwq0xBeyKs6hqEEy1JRwWqMg/p.png?fv_content=true&size_mode=5" />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
